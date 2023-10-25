8f5dd16cb3b1a8ed9de514405469314620174903
Leiluo <w13870987079@163.com>
2023-10-25 14:19:32 +0800

    证书生成函数优化

--- sm2cert.cpp
+++ sm2cert.cpp
@@ -12,11 +12,11 @@ Sm2Cert::~Sm2Cert()
 {
     delete ui;
 }
-
-std::shared_ptr<X509> Sm2Cert::genEncryptCert(std::shared_ptr<X509> midCA,
-                                              std::shared_ptr<EVP_PKEY> midcaPkey,
-                                              QString CNname,
-                                              QString days)
+std::shared_ptr<X509> Sm2Cert::genCert(int type,
+                                       std::shared_ptr<X509> midCA,
+                                       std::shared_ptr<EVP_PKEY> midcaPkey,
+                                       QString CNname,
+                                       QString days)
 {
     /* 生成用户密钥 */
     std::shared_ptr<EVP_PKEY> userKey(EVP_PKEY_Q_keygen(NULL, NULL, "SM2"), EVP_PKEY_free);
@@ -31,7 +31,11 @@ std::shared_ptr<X509> Sm2Cert::genEncryptCert(std::shared_ptr<X509> midCA,
     int len = BIO_pending(out.get());
     char buf[1024] = {};
     BIO_read(out.get(), buf, len);
-    this->ui->textBrowserEncryKey->setText(QString(buf));
+    if (type == 0) {
+        this->ui->textBrowserEncryKey->setText(QString(buf));
+    } else {
+        this->ui->textBrowserSignKey->setText(QString(buf));
+    }
     /* 生成CSR */
     std::shared_ptr<X509_REQ> userReq(X509_REQ_new(), X509_REQ_free);
     /* CSR相关设置 */
@@ -54,7 +58,12 @@ std::shared_ptr<X509> Sm2Cert::genEncryptCert(std::shared_ptr<X509> midCA,
     /* 签发证书 */
     std::shared_ptr<X509> userCer(X509_new(), X509_free);
     /* 证书相关设置 */
-    std::string str = "Key Encipherment, Data Encipherment";
+    std::string str;
+    if (type == 0) {
+        str = "Key Encipherment, Data Encipherment";
+    } else {
+        str = "Digital Signature";
+    }
     std::shared_ptr<X509_EXTENSION>
         cert_ex(X509V3_EXT_conf_nid(NULL, NULL, NID_key_usage, str.c_str()), X509_EXTENSION_free);
     X509_add_ext(userCer.get(), cert_ex.get(), -1);
@@ -78,78 +87,6 @@ std::shared_ptr<X509> Sm2Cert::genEncryptCert(std::shared_ptr<X509> midCA,
     std::shared_ptr<ASN1_TIME>
         rootAfterTime(ASN1_TIME_adj(NULL, curTime, 0, days.toInt() * 60 * 60 * 24), ASN1_TIME_free);
     X509_set_notAfter(userCer.get(), rootAfterTime.get());
-    /* 使用中间CA私钥签发 */
-    X509_sign(userCer.get(), midcaPkey.get(), EVP_sm3());
-
-    return userCer;
-}
-
-std::shared_ptr<X509> Sm2Cert::genSignCert(std::shared_ptr<X509> midCA,
-                                           std::shared_ptr<EVP_PKEY> midcaPkey,
-                                           QString CNname,
-                                           QString days)
-{
-    /* 生成用户密钥 */
-    std::shared_ptr<EVP_PKEY> userKey(EVP_PKEY_Q_keygen(NULL, NULL, "SM2"), EVP_PKEY_free);
-    if (userKey.get() == NULL) {
-        /* 错误处理 */
-        getError();
-        exit(0);
-    }
-    /* 输出用户私钥 */
-    std::shared_ptr<BIO> out(BIO_new(BIO_s_mem()), BIO_free);
-    PEM_write_bio_PrivateKey(out.get(), userKey.get(), NULL, 0, NULL, NULL, NULL);
-    int len = BIO_pending(out.get());
-    char buf[1024] = {};
-    BIO_read(out.get(), buf, len);
-    this->ui->textBrowserSignKey->setText(QString(buf));
-    /* 生成CSR */
-    std::shared_ptr<X509_REQ> userReq(X509_REQ_new(), X509_REQ_free);
-    /* CSR相关设置 */
-    X509_REQ_set_pubkey(userReq.get(), userKey.get());
-
-    std::shared_ptr<X509_NAME> userCAname(X509_NAME_new(), X509_NAME_free);
-    X509_NAME_add_entry_by_txt(userCAname.get(),
-                               "CN",
-                               MBSTRING_ASC,
-                               (unsigned char *) CNname.toStdString().c_str(),
-                               -1,
-                               -1,
-                               0);
-    X509_REQ_set_subject_name(userReq.get(), userCAname.get());
-
-    X509_REQ_set_version(userReq.get(), X509_VERSION_3);
-    X509_REQ_sign(userReq.get(), userKey.get(), EVP_sm3());
-    X509_REQ_verify(userReq.get(), userKey.get());
-
-    /* 签发证书 */
-    std::shared_ptr<X509> userCer(X509_new(), X509_free);
-    /* 证书相关设置 */
-    std::string str = "Digital Signature";
-    std::shared_ptr<X509_EXTENSION>
-        cert_ex(X509V3_EXT_conf_nid(NULL, NULL, NID_key_usage, str.c_str()), X509_EXTENSION_free);
-    X509_add_ext(userCer.get(), cert_ex.get(), -1);
-    //Key Encipherment, Data Encipherment
-    X509_set_version(userCer.get(), X509_VERSION_3);
-    X509_set_pubkey(userCer.get(), userKey.get());
-
-    std::shared_ptr<ASN1_INTEGER> aserial(ASN1_INTEGER_new(), ASN1_INTEGER_free);
-    ASN1_INTEGER_set(aserial.get(), 0);
-    X509_set_serialNumber(userCer.get(), aserial.get());
-
-    X509_set_subject_name(userCer.get(), userCAname.get());
-
-    const X509_NAME *rootCAname = X509_get_subject_name(midCA.get());
-    X509_set_issuer_name(userCer.get(), rootCAname);
-
-    time_t curTime = time(NULL);
-    std::shared_ptr<ASN1_TIME> rootBeforeTime(ASN1_TIME_new(), ASN1_TIME_free);
-    ASN1_TIME_set(rootBeforeTime.get(), curTime);
-    X509_set_notBefore(userCer.get(), rootBeforeTime.get());
-    std::shared_ptr<ASN1_TIME>
-        rootAfterTime(ASN1_TIME_adj(NULL, curTime, 0, days.toInt() * 60 * 60 * 24), ASN1_TIME_free);
-    X509_set_notAfter(userCer.get(), rootAfterTime.get());
-
     /* 使用中间CA私钥签发 */
     X509_sign(userCer.get(), midcaPkey.get(), EVP_sm3());
 
@@ -214,9 +151,9 @@ void Sm2Cert::on_pushButtonGen_clicked()
     fpkey.close();
 
     /* 生成用户签名证书 */
-    std::shared_ptr<X509> userSignCer = this->genSignCert(subca, pkey, CN, days);
+    std::shared_ptr<X509> userSignCer = this->genCert(1, subca, pkey, CN, days);
     /* 生成用户加密证书 */
-    std::shared_ptr<X509> userEncryptCer = this->genEncryptCert(subca, pkey, CN, days);
+    std::shared_ptr<X509> userEncryptCer = this->genCert(0, subca, pkey, CN, days);
     /* 将用户证书以PEM格式输出到输出栏 */
     std::shared_ptr<BIO> outSign(BIO_new(BIO_s_mem()), BIO_free);
     PEM_write_bio_X509(outSign.get(), userSignCer.get());
